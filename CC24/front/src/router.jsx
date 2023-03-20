8026f8b6b7f370f65aede9daf2265976cbf4a826
원채령 <fairy3530@naver.com>
2023-03-20 17:54:09 +0900

    S08P22C208-293 add : 회원가입, 로그인 라우팅

--- CC24/front/src/router.jsx
+++ CC24/front/src/router.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-import App from './App.jsx';
+// import App from './App.jsx';
+import Signup from '././pages/signup/Signup.jsx';
+import Index from '././pages/index/Index.jsx';
+import Login from './pages/login/Login.jsx';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <App />,
+    element: <Index />,
   },
-//   {
-//     path: "/login",
-//     element: <App />,
-// },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
 ]);
 
 export default router;
