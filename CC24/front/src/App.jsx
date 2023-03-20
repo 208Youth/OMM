3d5daaa9dcca2cfb15f76732f788d8d740fa6887
cjswp3550 <cjswp3550@naver.com>
2023-03-20 14:47:08 +0900

    S08P22C208-296 fix : eslint prettier 설정 수정

--- CC24/front/src/App.jsx
+++ CC24/front/src/App.jsx
@@ -1,6 +1,5 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import reactLogo from './assets/react.svg';
-// import viteLogo from '../../../../../../../vite.svg';
 import './App.css';
 
 function App() {
@@ -19,22 +18,17 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={() => setCount(() => count + 1)}>
           count is
-          {' '}
           {count}
         </button>
         <p>
           Edit
-          {' '}
           <code>src/App.jsx</code>
-          {' '}
           and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </div>
   );
 }
