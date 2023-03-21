3246c09b172f12d277bf8a8c6091f1c99f8e6f82
김윤미 <yoonmihy@naver.com>
2023-03-21 14:26:23 +0900

    S08P22C208-289 add : 대학교 인증

--- CC24/back/src/main/java/com/cc24/service/CertService.java
+++ CC24/back/src/main/java/com/cc24/service/CertService.java
@@ -1,18 +1,25 @@
 package com.cc24.service;
 
+import com.cc24.exception.CustomException;
+import com.cc24.model.dto.university.AuthInfoDto;
 import com.cc24.model.dto.university.response.UniversityDto;
+import com.cc24.model.entity.university.Student;
 import com.cc24.model.entity.university.University;
-import com.cc24.repository.CertRepository;
+import com.cc24.repository.university.StudentRepository;
+import com.cc24.repository.university.UniversityRepository;
+import com.cc24.util.error.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CertService {
-    private final CertRepository certRepository;
+    private final UniversityRepository universityRepository;
+    private final StudentRepository studentRepository;
 
     public List<UniversityDto> getUniversityList() {
         List<University> universities = universityRepository.findAll();
@@ -24,5 +31,17 @@ public class CertService {
                             .build());
         });
         return result;
+    }
+
+    public void getUniversityCert(AuthInfoDto authInfoDto, Long universityId) {
+        String name = authInfoDto.getName();
+        Date birthDate = authInfoDto.getBirthDate();
+
+        Student student = studentRepository.findByNameAndBirthDate(name, birthDate)
+                .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
+
+        if(student.getUniversity().getId() != universityId) {
+            throw new CustomException(ErrorCode.CANNOT_AUTHORIZE_MEMBER);
+        }
     }
 }
