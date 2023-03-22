cc939ca5a749df522de8630cf574b340cecc81f3
안수빈 <sbsc20@naver.com>
2023-03-22 15:58:57 +0900

    S08P22C208-287 add : 에러코드 추가

--- CC24/back/src/main/java/com/cc24/util/error/ErrorCode.java
+++ CC24/back/src/main/java/com/cc24/util/error/ErrorCode.java
@@ -13,8 +13,13 @@ public enum ErrorCode {
     MEMBER_NOT_FOUND(NOT_FOUND, "없는 사용자입니다."),
 
     /* 400 BAD_REQUEST : 잘못된 요청 */
-    CANNOT_AUTHORIZE_MEMBER(BAD_REQUEST, "인증에 실패했습니다.")
+    CANNOT_AUTHORIZE_MEMBER(BAD_REQUEST, "인증에 실패했습니다."),
+
+    /* 500 INTERNAL_SERVER_ERROR : 서버 문제 발생 */
+    DID_ERROR(INTERNAL_SERVER_ERROR, "did 처리 중 문제가 발생했습니다."),
+    SIGN_ERROR(INTERNAL_SERVER_ERROR, "서명 중 문제가 발생했습니다.")
     ;
+
     private final HttpStatus httpStatus;
     private final String detail;
 }
