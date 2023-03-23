602e6ddf3c2ad24c2b6fb8741a5793e8adb71067
원채령 <kimchae1205@gmail.com>
2023-03-24 00:11:07 +0900

    S08P22C208-301 add : index 페이지 버튼

--- OMM/front/src/pages/index/Index.jsx
+++ OMM/front/src/pages/index/Index.jsx
@@ -4,10 +4,20 @@ import './Index.css'
 function Index() {
     return (
         <div className='background'>
-            <div class="flex">
-                <img class="self-center" src="../../../public/ommlogo.png" alt="" />
+            <div className='logo'>
+                <div class="flex">
+                    <img class="self-center" src="../../../public/ommlogo.png" alt="" />
+                </div>
             </div>
-            <h1></h1>
+            <div className='buttons'>
+                <div class="grid justify-items-center">
+                    <h1 class="text-white text-3xl">맞소사.</h1>
+                    <h1 class="text-white text-3xl mt-2 mb-20">Oh My Match</h1>
+                    <button className='btn-white'>로그인</button>
+                    <button className='btn-transparent'>회원가입</button>
+                </div>
+
+            </div>
         </div>
     )
 }
