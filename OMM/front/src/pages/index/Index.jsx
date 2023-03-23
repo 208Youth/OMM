d09745481fe1adbc51513b114462a98f0d2847fc
원채령 <kimchae1205@gmail.com>
2023-03-23 22:32:44 +0900

    S08P22C208-301 add : index 페이지

---
+++ OMM/front/src/pages/index/Index.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Index.css'
+
+function Index() {
+    return (
+        <div className='background'>
+            <div class="flex">
+                <img class="self-center" src="../../../public/ommlogo.png" alt="" />
+            </div>
+            <h1></h1>
+        </div>
+    )
+}
+
+export default Index
