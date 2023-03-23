5adb0cb0b990c7db10c7f5581d54cd856e1c6a36
임양원 <gospel328@gmail.com>
2023-03-23 20:57:29 +0900

    S08P22C208-295 "ocr기본 기능 완성, 사진삭제 로직 구현 필요."

---
+++ CC24/fastapi/identification.py
@@ -0,0 +1,104 @@
+import io
+import os
+from google.cloud import vision
+from google.cloud.vision_v1 import types
+
+test = 123
+#  환경정보의 경로를 절대경로로 해야 실행 위치가 달라도 오류가 나지 않는다.
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = './ocr-origin-4d4cec7ffdde.json'
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+# Gets a list of filenames in the "book_img" directory
+
+# 1 로컬사진을 분석하는 코드
+def detect_text(path):
+    filenames = os.listdir('./iden_img')
+
+    filename = filenames[0]
+    path = os.path.join('./iden_img', filename)
+
+    # Loads the image into memory
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    # Creates a Vision API Image object
+    image = types.Image(content=content)
+
+    # Performs text detection on the image
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    
+    # print('\n"{}"'.format(texts[0].description))
+    # print("{}".format(texts[0].description))
+    # lines = texts.split("\n")
+    # doc_type = lines[0]
+    # name = lines[1].split("(")[0]
+    # id_num = lines[2]
+    # print(name)
+    doc_type = (texts[0].description)[0:5]
+    name = (texts[0].description)[6:9]
+    birthday = (texts[0].description)[15:21]
+    # sex = (texts[0].description)[22:23]
+    print(doc_type)
+    print(name)
+    print(birthday)
+    # print(sex)
+    return {'doc_type': doc_type, 'name': name, 'birthday': birthday}
+
+
+# 1.1 로컬상의 사진 분석
+# def detect_text(path):
+#     """Detects text in the file."""
+#     client = vision.ImageAnnotatorClient()
+#     path = os.path.join('../front/src/pages/signup/tmp_img', filename)
+
+#     with io.open(path, 'rb') as image_file:
+#         content = image_file.read()
+
+#     image = vision.Image(content=content)
+
+#     response = client.text_detection(image=image)
+#     texts = response.text_annotations
+#     print('Texts:')
+
+#     for text in texts:
+#         print('\n"{}"'.format(text.description))
+
+#         vertices = (['({},{})'.format(vertex.x, vertex.y)
+#                     for vertex in text.bounding_poly.vertices])
+
+#         print('bounds: {}'.format(','.join(vertices)))
+
+#     if response.error.message:
+#         raise Exception(
+#             '{}\nFor more info on error messages, check: '
+#             'https://cloud.google.com/apis/design/errors'.format(
+#                 response.error.message))
+
+# 2 인터넷 상의 사진을 분석하는 코드
+
+def detect_text_uri(uri):
+    """Detects text in the file located in Google Cloud Storage or on the Web.
+    """
+  
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image()
+    image.source.image_uri = uri
+
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+
+    # print(response)
+    return texts
+
+    # print('\n"{}"'.format(texts[0].description))
+    # return '\n"{}"'.format(texts[0].description)
+
+def plus2(a):
+    return a+ 2
+
+
+    
+    
+# detect_text_uri('https://ifh.cc/g/yfWH3y.jpg')
--- CC24/front/src/pages/signup/IdenModal.jsx
+++ CC24/front/src/pages/signup/IdenModal.jsx
@@ -1,7 +1,89 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 // import CloseBtn from '../../assets/CloseBtn.svg';
-import './index.css';
+// import './index.css';
 // import './FaceRecogModal.css';
+// import http from '../api/fastapi';
+
+// function Result() {
+//   return (
+//     <div>
+//       <div>
+//         <span>이름</span>
+//         <span>이름값</span>
+//         {/* <span>{이름값}</span> */}
+//       </div>
+//     </div>
+//   );
+// }
+
+// function IdenModal() {
+//   const [imageSrc, setImageSrc] = useState('');
+
+//   const encodeFileToBase64 = (fileBlob) => {
+//     const reader = new FileReader();
+//     reader.readAsDataURL(fileBlob);
+//     return new Promise((resolve) => {
+//       reader.onload = () => {
+//         setImageSrc(reader.result);
+
+//         resolve();
+//         const url = await fileOrBlobToUrl(imageSrc);
+//       };
+//     });
+//   };
+
+//   return (
+//     <div className="flex-col w-80 mx-auto">
+//       <p className="flex">
+//         <img src={CloseBtn} className="w-8 h-8 ml-auto mt-2" alt="닫기" />
+//       </p>
+//       <p className="text-3xl text-left ml-9 leading-relaxed">
+//         본인
+//         <br />
+//         확인
+//       </p>
+
+//       <div>
+//         {/* <img src="/public/upload.png" alt="Upload" /> */}
+//       </div>
+//       {!imageSrc && (
+//       <label htmlFor="imginput">
+//         <div className="fileinput" />
+//       </label>
+//       )}
+//       <input
+//         // className="fileinput"
+//         id="imginput"
+//         type="file"
+//         style={{ display: 'none' }}
+//         onChange={(e) => {
+//           encodeFileToBase64(e.target.files[0]);
+//           http
+//             .post('/ocr');
+//         }}
+//       />
+//       <div className="preview">
+//         {imageSrc
+//         && <img src={imageSrc} alt="preview-img" />}
+
+//       </div>
+//       <div>
+//         {imageSrc && Result()}
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default IdenModal;
+
+// =======================
+
+import React, { useState, useEffect } from 'react';
+import CloseBtn from '../../assets/CloseBtn.svg';
+import './index.css';
+import './FaceRecogModal.css';
+// import fastapi from '../../api/fastapi.js';
+import axios from 'axios';
 
 function Result() {
   return (
@@ -11,65 +93,100 @@ function Result() {
         <span>이름값</span>
         {/* <span>{이름값}</span> */}
       </div>
-      <div>
-        <span>이름</span>
-        <span>이름값</span>
-      </div>
-      <div>
-        <span>이름</span>
-        <span>이름값</span>
-      </div>
     </div>
   );
 }
 
 function IdenModal() {
   const [imageSrc, setImageSrc] = useState('');
+  const [imgfile, setFile] = useState('');
 
+  // fastapi의 idening를 실행시키기 위한 코드
+  async function sendImg() {
+    console.log(imgfile);
+    await axios({
+      method: 'post',
+      url: 'http://127.0.0.1:8000/idenimg',
+      data: {
+        // 데이터의 파일부분에 문제가 있는 것 같다.
+        file: imgfile,
+      },
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+      .then((res) => {
+        console.log(res.data);
+        console.log('fastapi로이미지를 보냈습니다.');
+      })
+      .catch((err) => {
+        console.log(err);
+        console.log('fastapi로 이미지를 보내는데 실패했습니다.');
+      });
+  }
   const encodeFileToBase64 = (fileBlob) => {
     const reader = new FileReader();
+
     reader.readAsDataURL(fileBlob);
+    setFile(fileBlob);
     return new Promise((resolve) => {
       reader.onload = () => {
         setImageSrc(reader.result);
+
         resolve();
       };
     });
   };
+  // 데이터가 변경되면 재렌더링 되게 하는 코드
+  useEffect(() => {
+    sendImg();
+  }, [imgfile]);
+  const handleFileInputChange = async (event) => {
+    const file = event.target.files[0];
+
+    await encodeFileToBase64(file);
+
+    // fastapi/iden_img에 이미지를 저장하는 코드를 써야한다.
+    const formData = new FormData();
+    formData.append('file', file);
+    // const { data } = await fastapi.get('/');
+
+    // fastapi.post('/idenimg', { file: imgfile });
+    // const { data } = await fastapi.post('/ocr');
+
+    // console.log(data); // 처리 결과 출력
+    // const { data } = await fastapi.post('/ocr', { path });
+  };
 
   return (
-    <div>
-      <div>
-        <h1>본인</h1>
-      </div>
-      <div>
-        <h1>확인</h1>
-      </div>
+    <div className="flex-col w-80 mx-auto">
+      <p className="flex">
+        <img src={CloseBtn} className="w-8 h-8 ml-auto mt-2" alt="닫기" />
+      </p>
+      <p className="text-3xl text-left ml-9 leading-relaxed">
+        본인
+        <br />
+        확인
+      </p>
+
       <div>
         {/* <img src="/public/upload.png" alt="Upload" /> */}
       </div>
       {!imageSrc && (
-      <label htmlFor="imginput">
-        <div className="fileinput" />
-      </label>
+        <label htmlFor="imginput">
+          <div className="fileinput" />
+        </label>
       )}
       <input
-        // className="fileinput"
         id="imginput"
         type="file"
         style={{ display: 'none' }}
-        onChange={(e) => {
-          encodeFileToBase64(e.target.files[0]);
-        }}
+        onChange={handleFileInputChange}
       />
       <div className="preview">
-        {imageSrc
-        && <img src={imageSrc} alt="preview-img" />}
-
+        {imageSrc && <img src={imageSrc} alt="preview-img" />}
       </div>
-      <div>
-        {imageSrc && Result()}
-      </div>
+      <div>{imageSrc && <Result />}</div>
     </div>
   );
 }
