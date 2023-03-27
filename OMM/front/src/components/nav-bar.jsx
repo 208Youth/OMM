ea8a6397433d4807f1c6bf07d4457378b22abfc0
원채령 <fairy3530@naver.com>
2023-03-27 23:24:58 +0900

    S08P22C208-460 add : 메인 페이지 레이아웃

--- OMM/front/src/components/nav-bar.jsx
+++ OMM/front/src/components/nav-bar.jsx
@@ -1,30 +1,31 @@
 import React from 'react';
 import './nav-bar.scss';
+import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-<div>
-    <div className="bottom-appbar">
-      <div className="tabs">
-        <div className="tab is-active tab--left">
-          <div className="flex justify-between">
-            <div>💬</div>
-            <div>💬</div>
-          </div>
-        </div>
-        <div className="tab tab--fab">
-          <div className="top">
-            <div className="fab">+</div>
-          </div>
-        </div>
-        <div className="tab tab--right">
-          <div className="flex justify-between">
-            <div>❤️️</div>
-            <div>❤️️</div>
-          </div>
-        </div>
-      </div>
+<div class="container">
+  <nav class="menu">
+		<Link href="#" class="menu-item">
+      <i class="bi bi-search-heart transition duration-300 hover:scale-125"></i>
+      <i class="bi bi-search-heart-fill scale-125"></i>
+		</Link>
+		<Link href="#" class="menu-item">
+      <i class="bi bi-chat-heart transition duration-300 hover:scale-125"></i>
+      <i class="bi bi-chat-heart-fill scale-125"></i>
+		</Link>
+    <div className='flex w-16 h-16 transition duration-500 hover:scale-110 bg-red-100 rounded-full mx-auto my-auto shadow-md'>
+      <img className="w-10 h-10 mx-auto my-auto" src="/ommheart.png" alt="" />
     </div>
+		<Link href="#" class="menu-item">
+      <i class="bi bi-bell transition duration-300 hover:scale-125"></i>
+      <i class="bi bi-bell-fill scale-125"></i>
+		</Link>
+		<Link to="/Myprofile" class="menu-item">
+      <i class="bi bi-person transition duration-300 hover:scale-125"></i>
+      <i class="bi bi-person-fill scale-125"></i>
+		</Link>
+	</nav>
 </div>
   );
 }
