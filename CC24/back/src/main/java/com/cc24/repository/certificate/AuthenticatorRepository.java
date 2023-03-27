4253148caf0bf23d3146f4dd7b0cd501d27557a6
김윤미 <yoonmihy@naver.com>
2023-03-27 15:17:47 +0900

    S08P22C208-445 fix : JPA Repository 수정

--- CC24/back/src/main/java/com/cc24/repository/certificate/AuthenticatorRepository.java
+++ CC24/back/src/main/java/com/cc24/repository/certificate/AuthenticatorRepository.java
@@ -3,9 +3,9 @@ package com.cc24.repository.certificate;
 import com.cc24.model.entity.certificate.Authenticator;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface AuthenticatorRepository extends JpaRepository<Authenticator, Long> {
-    Optional<Authenticator> findByNameAndBirthDate(String name, Date birthDate);
+    Optional<Authenticator> findByNameAndBirthDateAndGender(String name, LocalDate birthDate, String gender);
 }
--- CC24/back/src/main/java/com/cc24/repository/health/HealthRepository.java
+++ CC24/back/src/main/java/com/cc24/repository/health/HealthRepository.java
@@ -3,9 +3,9 @@ package com.cc24.repository.health;
 import com.cc24.model.entity.health.Health;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Date;
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface HealthRepository extends JpaRepository<Health, Long> {
-    Optional<Health> findByNameAndBirthDate(String name, Date birthDate);
+    Optional<Health> findByNameAndBirthDateAndGender(String name, LocalDate birthDate, String gender);
 }
