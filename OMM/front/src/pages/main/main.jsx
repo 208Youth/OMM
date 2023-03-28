d7c3b7cc590d25664cd6d8e9957a6b549dfef4ec
원채령 <fairy3530@naver.com>
2023-03-28 12:11:19 +0900

    S08P22C208-460 style : 메인페이지 CSS 수정

--- OMM/front/src/pages/main/main.jsx
+++ OMM/front/src/pages/main/main.jsx
@@ -7,7 +7,7 @@ function Main({mainNav}) {
   mainNav = true
   return (
     <div className='flex flex-col'>
-      <div className='w-16 h-16 transition duration-500 hover:scale-110 bg-red-100 rounded-full shadow-md justify-center mx-auto mt-10'>
+      <div className='z-20 w-16 h-16 transition duration-500 hover:scale-110 bg-red-100 rounded-full shadow-md justify-center mx-auto mt-10'>
         <img className="w-10 h-10 mx-auto mt-3 flex" src="/reverseheart.png" alt="" />
       </div>
       <div className="mt-20">
