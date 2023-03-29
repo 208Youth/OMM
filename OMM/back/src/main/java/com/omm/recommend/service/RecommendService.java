69aad0f82bef636da3387c0b6f93209c6ae00003
박성완 <swany0509@naver.com>
2023-03-29 23:52:09 +0900

    S08P22C208-452 add : recommend logic add

--- OMM/back/src/main/java/com/omm/recommend/service/RecommendService.java
+++ OMM/back/src/main/java/com/omm/recommend/service/RecommendService.java
@@ -1,5 +1,6 @@
 package com.omm.recommend.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.omm.exception.member.MemberExceptionCode;
 import com.omm.exception.member.MemberRuntimeException;
 import com.omm.model.entity.Filtering;
@@ -13,7 +14,12 @@ import com.omm.repository.MyInfoRepository;
 import com.omm.repository.RecommendDtoRepository;
 import com.omm.util.EnumToKNN;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 import java.util.List;
@@ -31,7 +37,7 @@ public class RecommendService {
 
     private final RecommendDtoRepository recommendDtoRepository;
 
-    public List<GetRecommendListResponseDto> getRecommendList(String currentMemberDidAddress) {
+    public GetRecommendListResponseDto getRecommendList(String currentMemberDidAddress) {
 
 
         try {
@@ -48,7 +54,7 @@ public class RecommendService {
             // 좋아요, 싫어요 보낸 유저 제외
             List<RecommendDto> filteredList = recommendDtoRepository.filteredMembers3(myInfo);
 
-            System.out.println(filteredList.size());
+//            System.out.println(filteredList.size());
             // 이 목록이 없을 경우, 필터링 상관 없이 좋아요나 싫어요를 보내지 않은 상대 최대 200개 선별
             if (filteredList.isEmpty()) {
                 filteredList = recommendDtoRepository.filteredMembers2(myInfo);
@@ -68,11 +74,11 @@ public class RecommendService {
             myKNN.put("drinkingStyle", EnumToKNN.filterDrinkingToKNN(myFilter.getDrinkingStyle()));
             myKNN.put("smokingStyle", EnumToKNN.filterSmokingToKNN(myFilter.getSmokingStyle()));
 
-            System.out.print("Mydata | ");
-            for ( String key : myKNN.keySet() ) {
-                System.out.print(key + " : " + myKNN.get(key) + " | ");
-            }
-            System.out.println();
+//            System.out.print("Mydata | ");
+//            for ( String key : myKNN.keySet() ) {
+//                System.out.print(key + " : " + myKNN.get(key) + " | ");
+//            }
+//            System.out.println();
 
             Map<Long, Map<String, Double>> users = new HashMap<>();
             filteredList.forEach((filtMem)->{
@@ -84,11 +90,11 @@ public class RecommendService {
                 user.put("drinkingStyle", EnumToKNN.infoDrinkingToKNN(filtMem.getDrinkingStyle()));
                 user.put("smokingStyle", EnumToKNN.infoSmokingToKNN(filtMem.getSmokingStyle()));
 
-                System.out.print("user | ");
-                for ( String key : user.keySet() ) {
-                    System.out.print(key + " : " + user.get(key) + " | ");
-                }
-                System.out.println();
+//                System.out.print("user | ");
+//                for ( String key : user.keySet() ) {
+//                    System.out.print(key + " : " + user.get(key) + " | ");
+//                }
+//                System.out.println();
 
                 users.put(filtMem.getMemberId(),user);
 
@@ -98,22 +104,26 @@ public class RecommendService {
             requestBody.put("myinfo", myKNN);
             requestBody.put("users", users);
 
-//            // FastAPI로 전송
-//            String url = "http://localhost:8000/recommend"; // fastAPI url
-//            RestTemplate restTemplate = new RestTemplate();
-//            HttpHeaders headers = new HttpHeaders();
-//            headers.setContentType(MediaType.APPLICATION_JSON);
-//            ObjectMapper mapper = new ObjectMapper();
-//
-//            HttpEntity<String> request = new HttpEntity<>(mapper.writeValueAsString(requestBody), headers);
-//            ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
-//            String responseBody = response.getBody();
-//            System.out.println(responseBody);
+            // FastAPI로 전송
+            String url = "http://localhost:8000/recommend"; // fastAPI url
+            RestTemplate restTemplate = new RestTemplate();
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            ObjectMapper mapper = new ObjectMapper();
 
+            HttpEntity<String> request = new HttpEntity<>(mapper.writeValueAsString(requestBody), headers);
+            ResponseEntity<GetRecommendListResponseDto> response = restTemplate.postForEntity(url, request, GetRecommendListResponseDto.class);
+            GetRecommendListResponseDto responseBody = response.getBody();
+
+
+//            List<Long> idList = responseBody.getUserList();
+//            idList.forEach((num) -> System.out.print(num + " "));
+//            System.out.println(idList.get(0) + idList.get(1));
+
+            return responseBody;
 
         } catch (Exception e) {
             throw new MemberRuntimeException(MemberExceptionCode.MEMBER_INPUT_TYPE_WRONG);
         }
-        return null;
     }
 }
