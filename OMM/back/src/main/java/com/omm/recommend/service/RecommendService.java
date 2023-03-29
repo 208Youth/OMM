21d706bdf288aed0202bb6bc196484db663c4654
박성완 <swany0509@naver.com>
2023-03-29 17:56:09 +0900

    S08P22C208-452 add : recommend logic testing

--- OMM/back/src/main/java/com/omm/recommend/service/RecommendService.java
+++ OMM/back/src/main/java/com/omm/recommend/service/RecommendService.java
@@ -30,7 +30,6 @@ public class RecommendService {
 
     public List<GetRecommendListResponseDto> getRecommendList(String currentMemberDidAddress) {
 
-        System.out.println("hello 1");
 
         try {
             Member member = memberRepository.findByDidAddress(currentMemberDidAddress)
@@ -41,25 +40,13 @@ public class RecommendService {
 
             Filtering myFilter = filteringRepository.findByMember(member)
                     .orElseThrow(() -> new MemberRuntimeException(MemberExceptionCode.MEMBER_FILTERING_NOT_EXISTS));
-            System.out.println("hello 2");
-
-            System.out.println(member.toString());
-            System.out.println(myInfo.toString());
-            System.out.println(myFilter.toString());
 
             // 유저가 지정한 필터링 조건에 맞는 사람 1차적 선별
             // 좋아요, 싫어요 보낸 유저 제외
-//            List<RecommendDto> filteredList = new ArrayList<>();
             List<RecommendDto> filteredList = recommendDtoRepository.filteredMembers(myInfo, myFilter);
-//            List<RecommendDto> filteredList = myInfoRepository.filteredMembers(myInfo, myFilter);
-            System.out.println("hello 3");
 
-            filteredList.forEach((filtered)->{
-                System.out.println(filtered.toString());
-            });
             System.out.println(filteredList.size());
-            System.out.println("hello 4");
-//            // 이 목록이 없을 경우, 필터링 상관 없이 좋아요나 싫어요를 보내지 않은 상대 최대 200개 선별
+            // 이 목록이 없을 경우, 필터링 상관 없이 좋아요나 싫어요를 보내지 않은 상대 최대 200개 선별
 //            if (filteredList.isEmpty()) {
 //                filteredList = myInfoRepository.findByFiltering2(myInfo, myFilter);
 //            }
@@ -68,12 +55,12 @@ public class RecommendService {
 //            if (filteredList.isEmpty()) {
 //                filteredList = myInfoRepository.findByFiltering3(myInfo, myFilter);
 //            }
-
-            // FastAPI 에 보낼 자료구조 생성
+//
+//             //FastAPI 에 보낼 자료구조 생성
 //            Map<String, Double> myKNN = new HashMap<>();
 //            myKNN.put("age", (myFilter.getAgeMax() * 1.0 - myFilter.getAgeMin()) / 2);
 //            myKNN.put("height", (myFilter.getHeightMax() * 1.0 - myFilter.getHeightMin()) / 2);
-////            myKNN.put("distance", 0.0);
+//            myKNN.put("distance", 0.0);
 //            myKNN.put("contactStyle", EnumToKNN.filterContactToKNN(myFilter.getContactStyle()));
 //            myKNN.put("drinkingStyle", EnumToKNN.filterDrinkingToKNN(myFilter.getDrinkingStyle()));
 //            myKNN.put("smokingStyle", EnumToKNN.filterSmokingToKNN(myFilter.getSmokingStyle()));
@@ -83,7 +70,7 @@ public class RecommendService {
 //                Map<String, Double> user = new HashMap<>();
 //                user.put("age", (double) filtMem.getAge());
 //                user.put("height", (double) filtMem.getHeight());
-////                user.put("distance", (double) filtMem.getDistance());
+//                user.put("distance", (double) filtMem.getDistance());
 //                user.put("contactStyle", EnumToKNN.infoContactToKNN(filtMem.getContactStyle()));
 //                user.put("drinkingStyle", EnumToKNN.infoDrinkingToKNN(filtMem.getDrinkingStyle()));
 //                user.put("smokingStyle", EnumToKNN.infoSmokingToKNN(filtMem.getSmokingStyle()));
@@ -105,7 +92,7 @@ public class RecommendService {
 //            ResponseEntity<String> response = restTemplate.postForEntity(url, request, String.class);
 //            String responseBody = response.getBody();
 //            System.out.println(responseBody);
-            // 현재 멤버 가상 페르소나 설정
+//             현재 멤버 가상 페르소나 설정
 //            KNNDto persona = KNNDto.builder()
 //                    .age((myFilter.getAgeMax() * 1.0 - myFilter.getAgeMin()) / 2)
 //                    .height((myFilter.getHeightMax() * 1.0 - myFilter.getHeightMin()) / 2)
