d193973d2336f6fb1598bdded3330ec2f40edac4
안수빈 <sbsc20@naver.com>
2023-03-30 16:49:31 +0900

    S08P22C208-450 fix : nodejs 기본 경로 수정

--- CC24/nodejs/index.js
+++ CC24/nodejs/index.js
@@ -1,9 +1,10 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
 const port = 4424;
-const dotenv = require('dotenv');
+const dotenv = require("dotenv");
 
+app.use("/api/node", express.static("public"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,8 +13,8 @@ dotenv.config();
 // const routes = require("./routes/");
 // app.use(routes);
 
-const credentialRouter = require('./routes/did.js');
-app.use('/node/did', credentialRouter);
+const credentialRouter = require("./routes/did.js");
+app.use("/api/node/did", credentialRouter);
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`);
