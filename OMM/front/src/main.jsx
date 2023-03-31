4a7fa108e155eeada77ae2085ea839a0b08f3e2c
원채령 <fairy3530@naver.com>
2023-03-31 22:12:07 +0900

    S08P22C208-466 fix : redux-persist 설정

--- OMM/front/src/main.jsx
+++ OMM/front/src/main.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { Provider } from 'react-redux';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import router from './router.jsx';
+import store from './store/store';
 
+let persistor = persistStore(store);
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <RouterProvider router={router} />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>,
 );
 
