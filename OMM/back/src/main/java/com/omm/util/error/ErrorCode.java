f2c7242b98731c1b9a9f30593a43071f0cf59282
박성완 <swany0509@naver.com>
2023-03-31 14:32:38 +0900

    S08P22C208-452 add : login logic

--- OMM/back/src/main/java/com/omm/util/error/ErrorCode.java
+++ OMM/back/src/main/java/com/omm/util/error/ErrorCode.java
@@ -11,6 +11,7 @@ import static org.springframework.http.HttpStatus.*;
 public enum ErrorCode {
     /* 404 NOT_FOUND : Resource 를 찾을 수 없음 */
     MEMBER_NOT_FOUND(NOT_FOUND, "없는 사용자입니다."),
+    PAGE_NOT_FOUND(NOT_FOUND, "존재하지 않은 페이지입니다."),
 
     /* 400 BAD_REQUEST : 잘못된 요청 */
     CANNOT_AUTHORIZE_MEMBER(BAD_REQUEST, "인증에 실패했습니다."),
