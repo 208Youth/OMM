89ccb5e9318abc0adac9c2965f9c54410cbae8d5
김윤미 <yoonmihy@naver.com>
2023-04-01 16:55:15 +0900

    S08P22C208-448 fix : 알림 목록 조회 api에 맞게 수정

--- OMM/back/src/main/java/com/omm/matching/model/dto/response/GetNotificationsResponseDto.java
+++ OMM/back/src/main/java/com/omm/matching/model/dto/response/GetNotificationsResponseDto.java
@@ -1,5 +1,6 @@
 package com.omm.matching.model.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -8,5 +9,6 @@ import java.util.List;
 @AllArgsConstructor
 @Getter
 public class GetNotificationsResponseDto {
+    @JsonProperty(value = "list")
     List<NotificationResponseDto> list;
 }
