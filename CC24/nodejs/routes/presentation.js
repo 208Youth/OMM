a7a46231f933e4f48f76fcaaf578a45addb38cc6
안수빈 <sbsc20@naver.com>
2023-04-01 22:12:28 +0900

    S08P22C208-455 fix : 경로 수정

--- CC24/nodejs/routes/presentation.js
+++ CC24/nodejs/routes/presentation.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const did = require("../module/did");
+const did = require("./module/did");
 
 // Verify VP, Return claims
 router.post("/", async (req, res) => {
