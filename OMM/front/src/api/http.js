dcd9ba847250a6190d8887fdcf505111a75a453f
최보영 <cjswp3550@naver.com>
2023-04-02 15:01:49 +0900

    S08P22C208-464 refactor : axios baseURL 주소 변경

--- OMM/front/src/api/http.js
+++ OMM/front/src/api/http.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 axios.defaults.withCredentials = false;
 
 export default axios.create({
-  baseURL: 'http://localhost:5000/api/',
+  baseURL: 'http://localhost:5000/api',
   headers: {
     'Content-Type': 'application/json;charset=utf-8',
     'Access-Control-Allow-Origin': '*',
