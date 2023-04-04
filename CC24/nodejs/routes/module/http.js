e7d21d204a26aef07f51d1d79747e813bdc90658
안수빈 <sbsc20@naver.com>
2023-04-04 14:04:41 +0900

    S08P22C208-617 fix : 에러 로그 출력

--- CC24/nodejs/routes/module/http.js
+++ CC24/nodejs/routes/module/http.js
@@ -1,17 +1,19 @@
-const http = require("http");
+const http = require('http');
 
 const sendHttpRequest = (data, options) => {
   return new Promise((resolve, reject) => {
     const req = http.request(options, (res) => {
       if (res.statusCode < 200 || res.statusCode >= 300) {
+        console.log(res.statusCode, res.statusMessage);
         reject(new Error(`HTTP Error: ${res.statusCode}`));
         return;
       }
-      res.on("data", (data) => {
+      res.on('data', (data) => {
         resolve(data.toString());
       });
     });
-    req.on("error", (error) => {
+    req.on('error', (error) => {
+      console.log(error);
       reject(error);
     });
     req.write(data);
