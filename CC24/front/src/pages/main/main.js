be166f4f3382782ced6e9b717f2194130c36c412
박성완 <swany0509@naver.com>
2023-04-04 20:39:34 +0900

    S08P22C208-610 add : 인증서 해독해서 보여주기

--- CC24/front/src/pages/main/main.js
+++ CC24/front/src/pages/main/main.js
@@ -1,64 +1,134 @@
 /* eslint-disable */
 import './Main.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../../components/Navbar';
 import { useSelector } from 'react-redux';
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/effect-cards";
-import { EffectCards } from "swiper";
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/effect-cards';
+import { EffectCards } from 'swiper';
 import { useState } from 'react';
+import jwt_decode from 'jwt-decode';
 
 function Main() {
+  const user = useSelector((state) => state.user);
   const cert = useSelector((state) => state.user.cert);
   const [change, setChange] = useState(true);
-  let certList = []
+  const contentList = [];
+  let certList = [];
+  // if (localStorage.getItem('IdenVC')) {
+  //   console.log(localStorage.getItem('IdenVC'));
+  //   certList.push('신분증');
+  //   contentList.push(
+  //     contentList.push(`${user.name}
+  //     ${user.year}-${user.month}-${user.day}
+  //     ${user.gender}`),
+  //   );
+  //   // contentList.push(jwt_decode(localStorage.getItem('IdenVC')));
+  // }
   if (localStorage.getItem('IdenVC')) {
     console.log(localStorage.getItem('IdenVC'));
-    certList.push('신분증')
+    certList.push('신분증');
+    let id = `
+    ${user.name} ${user.gender}\n
+    ${user.year}-${user.month}-${user.day}
+    `;
+    console.log(id);
+    contentList.push(id);
+    // contentList.push(jwt_decode(localStorage.getItem('IdenVC')));
   }
-  certList.push(...cert)
-  console.log(certList);
+  if (localStorage.getItem('VC')) {
+    const aaa = JSON.parse(localStorage.getItem('VC'));
+    aaa.forEach((element) => {
+      for (let [key, value] of Object.entries(element)) {
+        let decoded = jwt_decode(value).vc.credentialSubject;
+        let data = '';
+        switch (key) {
+          case 'UniversityCredential':
+            data = decoded.university.name;
+            break;
+          case 'JobCredential':
+            data = decoded.job.name;
+            break;
+          case 'IncomeCredential':
+            data = `${decoded.income.income} 원`;
+            break;
+          case 'EstateCredential':
+            let res = 0;
+            decoded.estate.estates.forEach((amount) => {
+              res += amount;
+            });
+            data = `${res} 원`;
+            break;
+          case 'HealthCredential':
+            data = decoded.health.date;
+            break;
+          case 'CertificateCredential':
+            data = decoded.certificate.name;
+            break;
+          default:
+            break;
+        }
+        contentList.push(data);
+        console.log(data);
+      }
+    });
+  }
+  certList.push(...cert);
+  // console.log(certList);
+
   return (
     <div className="flex px-10">
       <div className="flex-col w-80 mx-auto">
-      <div>
-        <p className="text-3xl text-left ml-9 mt-20 leading-relaxed" style={{ marginLeft: '1rem' }}>
-          인증서
-        </p>
-      </div>
-      <div>
-        <p className="text-3xl text-left ml-9 leading-relaxed" style={{ marginLeft: '1rem' }}>
-          리스트
-        </p>
-        <br />
-      </div>
-        <Swiper
-        effect={"cards"}
-        grabCursor={true}
-        modules={[EffectCards]}
-        className="mySwiper"
-        >
-        {certList.map((cert) => (
-          <SwiperSlide onClick={() => setChange(!change)}>
-            { change && <div>
-              <p className="text-3xl text-left leading-relaxed flex justify-start t-0">
-                <span className='inline'>{cert}
-                  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-shield-fill-check inline ml-2" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm2.146 5.146a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647z"/>
-                  </svg>
-                </span>
-              </p>
-            </div>}
-            { !change && <div>
-              <p className="text-3xl text-left leading-relaxed flex justify-start t-0">
-                내용
-              </p>
-            </div>}
-          </SwiperSlide>
-        ))}
-      </Swiper>
-
+        <div>
+          <p
+            className="text-3xl text-left ml-9 mt-20 leading-relaxed"
+            style={{ marginLeft: '1rem' }}
+          >
+            인증서
+          </p>
+        </div>
+        <div>
+          <p className="text-3xl text-left ml-9 leading-relaxed" style={{ marginLeft: '1rem' }}>
+            리스트
+          </p>
+          <br />
+        </div>
+        <Swiper effect={'cards'} grabCursor={true} modules={[EffectCards]} className="mySwiper">
+          {certList.map((cert, index) => (
+            <SwiperSlide key={index} onClick={() => setChange(!change)}>
+              {change && (
+                <div>
+                  <p className="text-3xl text-left leading-relaxed flex justify-start t-0">
+                    <span className="inline">
+                      {cert}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="16"
+                        height="16"
+                        fill="currentColor"
+                        class="bi bi-shield-fill-check inline ml-2"
+                        viewBox="0 0 16 16"
+                      >
+                        <path
+                          fill-rule="evenodd"
+                          d="M8 0c-.69 0-1.843.265-2.928.56-1.11.3-2.229.655-2.887.87a1.54 1.54 0 0 0-1.044 1.262c-.596 4.477.787 7.795 2.465 9.99a11.777 11.777 0 0 0 2.517 2.453c.386.273.744.482 1.048.625.28.132.581.24.829.24s.548-.108.829-.24a7.159 7.159 0 0 0 1.048-.625 11.775 11.775 0 0 0 2.517-2.453c1.678-2.195 3.061-5.513 2.465-9.99a1.541 1.541 0 0 0-1.044-1.263 62.467 62.467 0 0 0-2.887-.87C9.843.266 8.69 0 8 0zm2.146 5.146a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7.5 7.793l2.646-2.647z"
+                        />
+                      </svg>
+                    </span>
+                  </p>
+                </div>
+              )}
+              {!change && (
+                <div>
+                  <p className="text-3xl text-left leading-relaxed flex justify-start t-0">
+                    {contentList[index]}
+                  </p>
+                </div>
+              )}
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
       <Navbar className="fixed" />
     </div>
