4f599e339f0ca301f547c1c724ffce43893dbc77
sbsc20 <sbsc20@naver.com>
2023-04-05 23:37:01 +0900

    S08P22C208-617 fix : 멤버 이미지 출력

--- OMM/front/src/components/ChatRoom.jsx
+++ OMM/front/src/components/ChatRoom.jsx
@@ -51,7 +51,7 @@ function ChatRoom({ chat, moveTo }) {
       className="w-[312px] h-[4.7rem] flex p-3 bg-white bg-opacity-60 text-xs rounded-lg mb-1"
     >
       <div className="w-10 h-10 self-center rounded-full">
-        <img src={chat.other.image} alt="사진" />
+        <img src={`data:image/png;base64,${chat.other.image}`} alt="사진" />
         {/* <img src={Img} alt="사진" /> */}
       </div>
       <div className="self-center w-40 ml-3">
