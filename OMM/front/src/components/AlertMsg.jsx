b00b8c5e09f769db776db391c3a8ee8a678a47a8
김윤미 <yoonmihy@naver.com>
2023-04-06 22:47:28 +0900

    S08P22C208-448 fix : 채팅 테스트

--- OMM/front/src/components/AlertMsg.jsx
+++ OMM/front/src/components/AlertMsg.jsx
@@ -32,13 +32,13 @@ function AlertMsg({ msg, deletemsg }) {
   return (
     <div className="w-[312px] h-[4.7rem] flex p-3 bg-white bg-opacity-60 text-xs rounded-lg mb-1">
       <div className="w-10 h-10 self-center rounded-full object-cover">
-        {msg.sender.imageContent ? (<img
+        {msg.sender.imageContent ? <img
           src={`data:image/png;base64,${msg.sender.imageContent.imageContent}`}
           alt="사진"
-        />) : (<img
+        /> : <img
           src="/ommheart.png"
           alt="사진"
-        />)}
+        />}
       </div>
       <div className="self-center w-40 ml-3 font-sans">
         <span className="font-sans font-bold inline-block whitespace-nowrap overflow-hidden text-ellipsis">
