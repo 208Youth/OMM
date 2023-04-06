697ea68241b8dd1040a42293f91ccf93a38707c3
안수빈 <sbsc20@naver.com>
2023-04-06 22:53:49 +0900

    S08P22C208-617 add : 관리자 로그인 페이지 추가

---
+++ OMM/front/src/pages/admin/AdminLogin.jsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react';
+import http from '@/api/http';
+
+function LoginForm() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await http.post('authenticate/admin', {
+        username: username,
+        password: password,
+      });
+      localStorage.setItem('accesstoken', data.token);
+      setMessage('로그인 성공');
+    } catch (error) {
+      setMessage('로그인 실패');
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {message && <p>{message}</p>}
+      <div>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button type="submit">Log In</button>
+    </form>
+  );
+}
+
+export default LoginForm;
