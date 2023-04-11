a8e6ce6dd515c1aad11b01b64d68d39c4518266f
김윤미 <yoonmihy@naver.com>
2023-04-11 22:25:17 +0900

    S08P22C208-289 fix : 사진 깨짐
    Former-commit-id: d35c09748035420e29d5d7a4a28f427503533adf

--- OMM/front/src/components/AlertMsg.jsx
+++ OMM/front/src/components/AlertMsg.jsx
@@ -41,7 +41,7 @@ function AlertMsg({ msg, deletemsg }) {
       >
         {msg.sender.imageContent ? (
           <img
-            src={`data:image/png;base64,${msg.sender.imageContent}`}
+            src={`data:image/png;base64,${msg.sender.imageContent.imageContent}`}
             alt="사진"
           />
         ) : (
